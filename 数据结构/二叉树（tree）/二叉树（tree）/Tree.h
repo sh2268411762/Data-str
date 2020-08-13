e237671af4bafffb5a925753ee4145ea0ae44460
sh2268411762 <2268411762@qq.com>
2020-08-13 21:53:03 +0800

    二叉树

---
+++ 数据结构/二叉树（tree）/二叉树（tree）/Tree.h
@@ -0,0 +1,27 @@
+#ifndef _TREE_H_
+#define _TREE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+typedef int ElemType;
+//#pragma warning(disable:4996)
+//#include <vld.h>
+//void Swap(int *x, int *y)
+//{
+//	int z = *x;
+//	*x = *y;
+//	*y = z;
+//}
+
+
+
+#endif // !_TREE_H所有变量声明
+
